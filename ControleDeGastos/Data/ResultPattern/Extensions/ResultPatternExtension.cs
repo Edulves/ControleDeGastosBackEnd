6f1320648e0d78b6c9d4b231a96dcc4812ea6eba
Eduardo Alves <Eduardo.lves@outlook.com>
2026-05-02 18:54:52 -0300

    Feat new result pattern

---
+++ ControleDeGastos/Data/ResultPattern/Extensions/ResultPatternExtension.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeGastos.Data.PadraoDeResposta.Base;
+using Microsoft.AspNetCore.Mvc;
+namespace ControleDeGastos.Data.PadraoDeResposta.Extensao;
+
+public static class ResultPatternExtension
+{
+
+    public static IActionResult ToIActionResult<T>(this ResultPattern<T> result, ControllerBase controller)
+    {
+        if (result.IsSuccess)
+            return controller.Ok(result.Value);
+
+        var problem = new ProblemDetails
+        {
+            Status = result.StatusCode,
+            Title = result.Title,
+            Detail = result.Detail,
+            Instance = controller.HttpContext.Request.Path
+        };
+
+        return controller.StatusCode(problem.Status.Value, problem);
+    }
+}
